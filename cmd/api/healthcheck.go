7dd6735062f3ff3cd1842ed08bf4bdfb83788957
Addie Vasquez <2020151434@ub.edu.bz>
2025-08-25 14:49:42 -0600

    finished encoding json v2

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,19 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
-// healthcheckHandler gives us the health of the system (healthcheck.go)
-func (app *application) healthcheckHandler(w http.ResponseWriter, 
-	r *http.Request) {
+//regular go structure for dependencies
+func (a * application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Create a map to hold the response data
+	response := map[string]string{
+		"status":      "available",
+		"environment": a.config.env,
+		"version":     version,
+	}
 
-js := `{"status": "available", "environment": %q, "version": %q}`
-js = fmt.Sprintf(js, app.config.env, version)
-// Content-Type is text/plain by default
-w.Header().Set("Content-Type", "application/json")
-// Write the JSON as the HTTP response body.
-w.Write([]byte(js))
-
+	jsResponse, err := json.Marshal(response)
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+	jsResponse = append(jsResponse, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsResponse)
 }
