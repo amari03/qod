2bd0fb2c99efc1f15628b841f75d870c649a6a57
Addie Vasquez <2020151434@ub.edu.bz>
2025-08-25 20:35:24 -0600

    finished encoding JSON v5

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,13 +8,15 @@ import (
 //regular go structure for dependencies
 func (a * application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a map to hold the response data
-	response := map[string]string{
-		"status":      "available",
-		"environment": a.config.env,
-		"version":     version,
-	}
+	data := envelope {
+		"status": "available",
+		"system_info": map[string]string{
+				"environment": a.config.env,
+				"version": version,
+	   },
+}
 
-	err := a.writeJSON(w, http.StatusOK, response, nil)
+	err := a.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		a.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
