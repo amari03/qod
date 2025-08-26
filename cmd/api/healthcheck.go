d45624ca724360adb55e9c4d2e4d37eb65dbdbc5
Addie Vasquez <2020151434@ub.edu.bz>
2025-08-25 20:49:39 -0600

    finished encoding JSON v6

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -18,8 +18,6 @@ func (a * application) healthcheckHandler(w http.ResponseWriter, r *http.Request
 
 	err := a.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		a.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
-	}
+		a.serverErrorResponse(w, r, err)
+	   }	
 }
